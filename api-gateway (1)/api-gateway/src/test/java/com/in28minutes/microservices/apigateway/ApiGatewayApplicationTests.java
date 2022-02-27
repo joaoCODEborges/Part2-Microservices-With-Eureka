182f37ff5a423d5760115d29972805bc0837332d
JYB <jyb@phoebussoftware.com>
2022-02-27 17:03:33 +0000

    "working on route structure and git"

--- api-gateway (1)/api-gateway/src/test/java/com/in28minutes/microservices/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway (1)/api-gateway/src/test/java/com/in28minutes/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.apigateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
